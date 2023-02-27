321eda6d4b813dd96e864f1f0690882f30a1abc7
Pavle Zorić <97367273+pakizoricc@users.noreply.github.com>
2023-02-27 18:37:34 +0100

    Update db.php

--- sources/db.php
+++ sources/db.php
@@ -1,8 +1,5 @@
 <?php
-    // Enter your host name, database username, password, and database name.
-    // If you have not set database password on localhost then set empty.
     $conn = mysqli_connect("localhost","root","","fin_travel");
-    // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
