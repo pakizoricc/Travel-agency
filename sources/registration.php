8f731212827f32b31ac8a7e4f3b4ef2508c71ee6
Pavle Zorić <97367273+pakizoricc@users.noreply.github.com>
2023-02-27 18:32:49 +0100

    Update registration.php

--- sources/registration.php
+++ sources/registration.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 
-// initializing variables
 $first_name = "";
 $last_name = "";
 $username = "";
@@ -10,20 +9,19 @@ $password_1 = "";
 $password_2 = "";
 $errors = array(); 
 
-// connect to the database
+//konekcija ka bazi
 $conn = mysqli_connect('localhost', 'root', '', 'fin_travel');
 
-// REGISTER USER
+//registracija
 if (isset($_POST['reg_user'])) {
-  // receive all input values from the form
+  //ulazni podaci
   $first_name = $_POST['first_name'];
   $last_name = isset($_POST['last_name']) ? $_POST['last_name'] : "";
   $username = $_POST['username'];
   $email = $_POST['email'];
   $password_1 = $_POST['password_1'];
   $password_2 = $_POST['password_2'];
-  // form validation: ensure that the form is correctly filled ...
-  // by adding (array_push()) corresponding error unto $errors array
+  
   if (empty($first_name)) { $errors['first_name'] = "Ime nije popunjeno"; }
   if (empty($last_name)) { $errors['last_name'] = "Prezime nije popunjeno"; }
   if (empty($username)) { $errors['username'] = "Korisničko ime nije popunjeno"; }
@@ -34,29 +32,25 @@ if (isset($_POST['reg_user'])) {
   }
 }
 
-
-  // first check the database to make sure 
-  // a user does not already exist with the same username and/or email
+  //da li postoji nalog vec, provera na osnovu username i email polja
   $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
   $result = mysqli_query($conn, $user_check_query);
   $user = mysqli_fetch_assoc($result);
 
-  // Finally, register user if there are no errors in the form
   if (count($errors) == 0) {
-    // encrypt the password before saving in the database
+    //hash-ovanje sifre
     $password = md5($password_1);
-  
-    // insert user into database
+    
+    //unos podataka korisnika u bazu
     $query = "INSERT INTO users (first_name, last_name, username, email, password) 
               VALUES('$first_name', '$last_name', '$username', '$email', '$password')";
     mysqli_query($conn, $query);
   
-    // set session variables
     $_SESSION['username'] = $username;
     $_SESSION['registration_complete'] = true;
     $_SESSION['success'] = "You are now logged in";
   
-    // redirect to homepage
+    //redirekcija na pocetna.php
     header('location: ../pocetna.php');
     exit();
   }
